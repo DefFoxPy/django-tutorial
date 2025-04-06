c67a31f9c06628b4c101f87be25cc47cc021d0a8
DefFoxPy <juanmpy05@gmail.com>
2025-04-06 16:35:45 -0400

    Agrega archivos html y los carga por medio de un comando llamado render

--- djangotutorial/myapp/views.py
+++ djangotutorial/myapp/views.py
@@ -1,22 +1,24 @@
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Index page")
+	return render(request, 'index.html')
 
 def hello(request, username):
 	return HttpResponse('<h2>Hello %s<h2>' % username)
 
 def about(request):
-	return HttpResponse("About")
+	return render(request, 'about.html')
 
 def projects(request):
 	projects = list(Project.objects.values())
-	return JsonResponse(projects, safe=False)
+	#return JsonResponse(projects, safe=False)
+	return render(request, 'projects.html')
 
-def tasks(request, id):
+def tasks(request):
 	# task = Task.objects.get(pk=id) # consulta normal
-	task = get_object_or_404(Task, id=id)
-	return HttpResponse("task: %s" % task.title)
+	#task = get_object_or_404(Task, id=id)
+	#return HttpResponse("task: %s" % task.title)
+	return render(request, 'tasks.html')
