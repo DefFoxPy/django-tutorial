cffe52d5dedb6295ac4fd5ff2536045fcdbdc402
DefFoxPy <juanmpy05@gmail.com>
2025-04-08 17:40:33 -0400

    Muestra las tareas de cada proyecto

--- djangotutorial/myapp/views.py
+++ djangotutorial/myapp/views.py
@@ -58,6 +58,8 @@ def create_project(request):
 def project_detail(request, id):
 	#project = Project.objects.get(id=id)
 	project = get_object_or_404(Project, id=id)
+	tasks = Task.objects.filter(project_id=id)
 	return render(request, 'projects/detail.html', {
-		'project': project
+		'project': project,
+		'tasks': tasks
 	})
