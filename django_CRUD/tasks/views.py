df815380dbf048da767a369d4638ebadc9d4ec40
DefFoxPy <juanmpy05@gmail.com>
2025-04-09 16:07:00 -0400

    feat: Configura el signup para poder crear a un usuario

--- django_CRUD/tasks/views.py
+++ django_CRUD/tasks/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
 	return render(request, 'home.html')
 
 def signup(request):
-	return render(request, 'signup.html', {
+	if request.method == 'GET':
+		return render(request, 'signup.html', {
 		'form': UserCreationForm()
-	})
+		})
+	else:
+		if request.POST['password1'] == render.POST['password2']:	
+			try:
+				user = User.objects.create_user(username=request.POST['username'],
+										        password=request.POST['password1']
+				)
+				user.save()
+				return HttpResponse('User created successfully')
+			except:
+				return HttpResponse('Username already exits')
+
+		return HttpResponse('Password do not match')
+		
