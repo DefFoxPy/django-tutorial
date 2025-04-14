31c2881135111b9f26d28bf0c00cf8dfe95a852e
DefFoxPy <juanmpy05@gmail.com>
2025-04-14 11:24:21 -0400

    feat: Primer paso para mostrar cada tarea separada en una página.
    tambien crea un enlace directo hacía la tarea

--- django_CRUD/tasks/views.py
+++ django_CRUD/tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
@@ -59,6 +59,12 @@ def create_task(request):
 			'form': TaskForm(),
 			'error': 'Please provide valida date'
 		})
+
+def task_detail(request, task_id):
+	task = get_object_or_404(Task, pk=task_id)
+	return render(request, 'task_detail.html', {
+		'task': task 
+	})
 
 def signout(request):
 	logout(request)	
